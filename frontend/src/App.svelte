6b2839c98003407a0b05e29819f24bd003978084
Bruno Guberfain do Amaral <bruno.do.amaral@gmail.com>
2021-01-29 19:10:25 -0300

    Changed one line of CSS...

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -174,7 +174,7 @@
     .results {
 		width: 100%;
 		display: grid;
-		grid-template-columns: repeat(5, 1fr);
+		grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
 		grid-gap: 8px;
     }
 
