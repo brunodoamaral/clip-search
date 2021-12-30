76a22e8d618575e2e41b8fd32a2f44b04cf31ac3
Bruno Guberfain do Amaral <bruno.do.amaral@gmail.com>
2021-12-30 13:28:29 -0300

    Better port handling in dev mode

--- app.py
+++ app.py
@@ -125,13 +125,13 @@ if __name__ == '__main__':
 
     # Add dev env
     if args.dev:
-        print('Go to ./frontend folder and run: PORT=5001 npm install && npm run dev')
+        print('Go to ./frontend folder and run: npm install && npm run dev')
 
         @app.route('/', methods=['GET', 'POST'])
         def _proxy(*args, **kwargs):
             resp = requests.request(
                 method=request.method,
-                url=request.url.replace(request.host_url, 'http//localhost:5001'),
+                url=request.url.replace(request.host_url, 'http//localhost:8000'),
                 headers={key: value for (key, value) in request.headers if key != 'Host'},
                 data=request.get_data(),
                 cookies=request.cookies,
